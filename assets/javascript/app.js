29ecd3b83caf1e481d956ff5bfec591a6af6ef4f
Carl Vega <carljasonvega@gmail.com>
2018-10-09 14:11:41 -0400

    working timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,190 +1,131 @@
 $(document).ready(function() {
-  // variables
-  {
-    ("trivia_categories");
-    [
-      {
-        id: 9,
-        name: "General Knowledge"
-      },
-      {
-        id: 10,
-        name: "Entertainment: Books"
-      },
-      {
-        id: 11,
-        name: "Entertainment: Film"
-      },
-      {
-        id: 12,
-        name: "Entertainment: Music"
-      },
-      {
-        id: 13,
-        name: "Entertainment: Musicals & Theatres"
-      },
-      {
-        id: 14,
-        name: "Entertainment: Television"
-      },
-      {
-        id: 15,
-        name: "Entertainment: Video Games"
-      },
-      {
-        id: 16,
-        name: "Entertainment: Board Games"
-      },
-      {
-        id: 17,
-        name: "Science & Nature"
-      },
-      {
-        id: 18,
-        name: "Science: Computers"
-      },
-      {
-        id: 19,
-        name: "Science: Mathematics"
-      },
-      {
-        id: 20,
-        name: "Mythology"
-      },
-      {
-        id: 21,
-        name: "Sports"
-      },
-      {
-        id: 22,
-        name: "Geography"
-      },
-      {
-        id: 23,
-        name: "History"
-      },
-      {
-        id: 24,
-        name: "Politics"
-      },
-      {
-        id: 25,
-        name: "Art"
-      },
-      {
-        id: 26,
-        name: "Celebrities"
-      },
-      {
-        id: 27,
-        name: "Animals"
-      },
-      {
-        id: 28,
-        name: "Vehicles"
-      },
-      {
-        id: 29,
-        name: "Entertainment: Comics"
-      },
-      {
-        id: 30,
-        name: "Science: Gadgets"
-      },
-      {
-        id: 31,
-        name: "Entertainment: Japanese Anime & Manga"
-      },
-      {
-        id: 32,
-        name: "Entertainment: Cartoon & Animations"
-      }
-    ];
-  }
+  // default time is 30 seconds per question
+  var DEFAULT_TIME = 5 * 1000;
+  var DISPLAY_ANSWER_TIME = 2 * 1000;
+  var questions;
+  var currentQuestion;
+  var timeOption;
 
-  var param = {
-    cat: "",
-    diff: "",
-    type: "",
-    num: 10
-  };
-  $("#categories").on("click", function(event) {
-    param.cat = $("#categories").val();
-    console.log($("#categories").val());
-  });
-  $("#difficulty").on("click", function(event) {
-    param.diff = $("#difficulty").val();
-    console.log($("#difficulty").val());
-  });
-  $("#type").on("click", function(event) {
-    param.type = $("#type").val();
-    console.log($("#type").val());
-  });
-  $("#").on("click", function(event) {
-    param.num = $("#number").val();
-    console.log($("#number").val());
-  });
+  function buildUrl() {
+    // variables
+    // options object
+    var queryUrl = "https://opentdb.com/api.php";
+    var param = {
+      cat: $("#categories").val(),
+      type: $("#type").val(),
+      diff: $("#difficulty").val(),
+      num: $("#quantity").val() || 10
+    };
 
-  var response;
-  var url = "https://opentdb.com/api_count.php";
-  var query = "";
-
-  /*
-  var query = [];
-  if(cat) query.push("category=" + cat);
-  if()
-  if(type) query.push("type=" + type);
-  if(query.length) url += '?' + query.join('&');
-// gives https://opentdb.com/api_count.php?category=sports&type=multiple
-
-
-*/
-  if (param.cat) {
-    query.push("category=" + param.cat);
-  }
-  if (param.diff) {
-    query.push("difficulty=" + param.diff);
-  }
-  if (param.type) {
-    query.push("type=" + param.type);
-  }
-  if (param.num) {
-    query.push("amount=" + param.num);
-  }
-  if (query.length) {
-    url += "?" + query.join("&");
-  }
-
-  $.ajax({
-    url: url,
-    method: "GET"
-  })
-    .done(function(response) {})
-    .fail(function(err) {
-      throw err;
-    });
-
-  // functions
-  function start() {
-    var startDiv;
-
-    for (i = 0; i < trivia_categories.length; i++) {
-      startDiv = $("<button class='category'>");
-      var catID = trivia_categories[i].id();
-      var catName = trivia_categories[i].name();
-      startDiv.attr("id", catID);
-      startDiv.append(catName);
-      $("#start").append(startDiv);
+    var query = [];
+    // building api url
+    if (param.cat) {
+      query.push("category=" + param.cat);
     }
+    if (param.diff) {
+      query.push("difficulty=" + param.diff);
+    }
+    if (param.type) {
+      query.push("type=" + param.type);
+    }
+    if (param.num) {
+      query.push("amount=" + param.num);
+    }
+    if (query.length) {
+      queryUrl += "?" + query.join("&");
+    }
+    return queryUrl;
   }
-  // $("#categories").on("click", function()) {}
-  // function pickTrivia() {}
 
-  // function triviaAnswers() {}
+  // fills out html - handles asking the next question the questions array
+  function askQuestion() {
+    // get the current question
+    var question = questions[currentQuestion];
+    var answers = [].concat(question.incorrect_answers);
 
-  // function timer() {}
+    // randomly insert the right answer into the list of answers
+    var insert = Math.random(0, answers.length);
+    answers.splice(insert, 0, question.correct_answer);
 
-  // expressions
-  //   $(".dropdown").on("click", dropdown(event));
-  // start();
+    // display the question and answers
+    $("#question").html(question.question);
+    $("#answers").empty();
+    for (var i = 0; i < answers.length; i++) {
+      $("#answers").append(
+        $("<button>")
+          .addClass("list-group-item list-group-item-action")
+          .html(answers[i])
+      );
+    }
+    $(".list-group-item").on("click", pickAnswer);
+    timer = startTimer();
+  }
+
+  function startTimer() {
+    return setTimeout(function() {
+      ranOutOfTime();
+    }, timeOption || DEFAULT_TIME);
+  }
+
+  function pickAnswer(event) {
+    clearTimeout(timer);
+    var answer = $(event.target).html();
+    var rightAnswer = questions[currentQuestion].correct_answer;
+    if (answer === rightAnswer) correct();
+    else incorrect();
+  }
+
+  function ranOutOfTime() {
+    $("#answers").html("Ran out of time!");
+    nextQuestion();
+  }
+
+  function correct() {
+    $("#answers").html("Correct!");
+    nextQuestion();
+  }
+
+  function incorrect() {
+    $("#answers").html(
+      "Incorrect! Answer was " + questions[currentQuestion].correct_answer
+    );
+    nextQuestion();
+  }
+
+  function nextQuestion() {
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+      return gameOver();
+    }
+
+    // show right/wrong for a couple seconds before moving on
+    setTimeout(function() {
+      askQuestion();
+    }, DISPLAY_ANSWER_TIME);
+  }
+
+  function gameOver() {
+    $("#question").html("Game over!");
+    $("#answers").empty();
+  }
+
+  function start() {
+    // api url query input
+    var url = buildUrl();
+    console.log("Requesting URL " + url);
+
+    $.ajax({
+      url: url,
+      method: "GET"
+    }).done(function(response) {
+      questions = response.results;
+      currentQuestion = 0;
+      timeOption = $("#timer").val();
+      askQuestion();
+      $("#trivia").addClass("d-none");
+      $("#q-a").removeClass("d-none");
+    });
+  }
+
+  $("#start").on("click", start);
 });
